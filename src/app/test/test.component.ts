54679f7ccdc300ba08c129eb1c615ffe792a5b59
Kakarla <329191@NTTDATA.COM>
2024-03-22 16:21:45 -0700

    [ADD]test component added

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      test works!
+    </p>
+  `,
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+
+}
