d62e758c3f01eb034928fc1ac672eddd410e21c9
Kakarla <329191@NTTDATA.COM>
2024-03-22 16:28:50 -0700

    [ADD]test component added to the root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home.component';
+import { TestComponent } from './test/test.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, TestComponent],
   template: `<h1>Hello world!</h1>
-  <div><home-root></home-root></div>`,
+  <div><home-root></home-root></div>
+  <div><app-test></app-test></div>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
